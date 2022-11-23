2a72709e1202003c115fed09d8bf81e4f229f829
Stepan Serebryakov <serebryakov.s@dss.kz>
2022-11-23 23:04:33 +0600

    import AccessPointInspect

--- ClientsController.py
+++ ClientsController.py
@@ -3,13 +3,14 @@
 # https://github.com/SerebryakovS/Fau2ClientsController.git
 
 import argparse
+import AccessPointInspect
 
 if __name__ == "__main__":
     ArgumentsParser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter)
     ArgumentsParser.add_argument("-i", "--inspect", metavar="INSPECT", help = "Inspect all provided access points for speed and power")
     Arguments = vars(ArgumentsParser.parse_args())
     if Arguments["inspect"]:
-        InspectAccessPoints(Arguments["inspect"])
+        AccessPointInspect.InspectAccessPoints(Arguments["inspect"])
 
 
 
